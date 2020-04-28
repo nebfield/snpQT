f5f9c41e038cf75676c164d7d60e8bb0553e3d64
Benjamin Wingfield <b.wingfield@ulster.ac.uk>
2020-04-28 14:07:45 +0100

    update install scripts

--- scripts/snpqt_db_install_scripts/install_db.sh
+++ scripts/snpqt_db_install_scripts/install_db.sh
@@ -5,18 +5,18 @@
 set -e
 set -u 
 
-NXF_WORK=$(realpath "../../work") # keep work directories at top level 
+export NXF_WORK=$(realpath "../../work") # keep work directories at top level 
 SNPQT_CONFIG=$(realpath "../nextflow.config")
 PIPELINE_PATH=$(realpath "make_db/make_db.nf")
-IN_VCF=$SNPQT_DB_DIR+'/ALL.2of4intersection.20100804.genotypes.vcf.gz'
+IN_VCF=$SNPQT_DB_DIR'/ALL.2of4intersection.20100804.genotypes.vcf.gz'
 VCF_PATH=$(realpath $IN_VCF)
 
-if [[ ! -f "$VCF_PATH"]]; then
+if [ ! -f "$VCF_PATH" ]; then
     echo "$VCF_PATH doesn't exist. Did you run download_db.sh? Is \$SNPQT_DB_DIR set?"
     exit 1
 fi
 
 nextflow run $PIPELINE_PATH \
   -c $SNPQT_CONFIG \
-  --indir $SNPQT_DB_DIR
+  --indir $SNPQT_DB_DIR \ 
   -resume 
--- scripts/snpqt_db_install_scripts/make_db/make_db.nf
+++ scripts/snpqt_db_install_scripts/make_db/make_db.nf
@@ -33,7 +33,7 @@ process make_unique_ids {
     shell:
     '''
     plink --bfile ALL.2of4intersection.20100804.genotypes \
-      --set-missing-var-ids @:#[b37]$1,$2 --make-bed \
+      --set-missing-var-ids @:#[b37]\\$1,\\$2 --make-bed \
       --out 2of4intersection.20100804.genotypes_NMIDs
     '''
 }
