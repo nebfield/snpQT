394d2e39a3d9b88d8487eac90a06397bc41df2f3
Benjamin Wingfield <b.wingfield@ulster.ac.uk>
2020-04-29 18:48:24 +0100

    fix brain fart

--- scripts/01_samplevariant/bin/plot_maf.R
+++ scripts/01_samplevariant/bin/plot_maf.R
@@ -15,18 +15,7 @@ read.table(args[[1]], header=T) %>%
 ggplot(maf, aes(x = MAF)) +
   geom_histogram() +
   theme_classic() + 
-  xlab("Major allele frequency") + 
+  xlab("Minor allele frequency") + 
   ylab("Variant count") -> maf_hist
 
-n_maf <- nrow(maf)
-ggplot(maf, aes(x = plink, y = MAF)) +
-  geom_jitter(alpha=0.1) +
-  theme_classic() + 
-  ylab("Proportion of sample missing per SNP") +
-  xlab(glue::glue("Variant (n = {n_maf})")) +
-  theme(axis.text.y=element_blank(),
-        axis.ticks.y=element_blank()) + 
-  coord_flip() -> maf_scatter
-
-ggsave("maf.png", arrangeGrob(maf_hist, 
-  maf_scatter, ncol=2), dpi = 300)
+ggsave("maf.png", dpi = 300)
