6b5df413b39c364c08367fac1c198ab608fa2616
Benjamin Wingfield <b.wingfield@ulster.ac.uk>
2020-05-04 17:27:57 +0100

    add GRCh37

--- scripts/snpqt_db_install_scripts/download_db.sh
+++ scripts/snpqt_db_install_scripts/download_db.sh
@@ -8,6 +8,7 @@ set -e
 LIBRARY_DIR="$SNPQT_DB_DIR"
 gt_url="ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/release/20100804/ALL.2of4intersection.20100804.genotypes.vcf.gz"
 panel_url="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20100804/20100804.ALL.panel"
+human_url="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.gz"
 
 mkdir -p $LIBRARY_DIR
 cd $LIBRARY_DIR
@@ -16,6 +17,9 @@ if [ ! -e "download.complete" ]
     then
     echo "Downloading thousand genomes population information..."
     wget $panel_url
+    echo " finished."
+    echo "Downloading GRCh37..."
+    wget $human_url
     echo " finished."
     echo "Downloading thousand genomes genotypes (~60GB)..."
     wget $gt_url 
@@ -39,3 +43,5 @@ sed -i 's/GBR/EUR/g' 1kG_race.txt
 sed -i 's/FIN/EUR/g' 1kG_race.txt
 sed -i 's/CHS/ASN/g' 1kG_race.txt
 sed -i 's/PUR/AMR/g' 1kG_race.txt
+
+bgzip -d human_g1k_v37.fasta.gz
