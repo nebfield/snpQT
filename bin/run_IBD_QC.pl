c5e326c2b21b596d6cd9337490cf1ecf8ae9bdc8
Benjamin Wingfield <b.wingfield@ulster.ac.uk>
2021-01-19 14:17:17 +0000

    use command line arguments for file input
    Former-commit-id: 0ad8eba70e0e0f80708bfafb550c0ab511579953

--- bin/run_IBD_QC.pl
+++ bin/run_IBD_QC.pl
@@ -2,11 +2,24 @@
 
 use strict;
 
+# Store first argument (plink.imiss to variable $imiss and pihat_min0.125.genome to $pihat_file, from the command line
+my ($imiss_file, $pihat_file)=  @ARGV;
+
+# Checking if the arguments are passed
+if (not defined $imiss_file) {
+  die ".imiss file is not defined\n";
+}
+
+if (not defined $pihat_file) {
+  die ".genome file is not defined\n";
+}
+
 my %imiss;
 my %removed;
 
-open IMISS, '<', "plink.imiss"
-        or die "Cannot open plink.imiss: $!\n";
+
+open IMISS, '<', $imiss_file
+        or die "Cannot open genotypes file (pihat_0.125.genome): $!\n";
 print "Reading PLINK .imiss file plink.imiss\n";
 while(<IMISS>){
 	s/^\s+//;
@@ -14,14 +27,16 @@ while(<IMISS>){
     $imiss{$fields[0]}{$fields[1]} = $fields[5];
 }
 
-open GENOME, '<', "pihat_0.125.genome"
+open GENOME, '<',  $pihat_file
         or die "Cannot open genotypes file (pihat_0.125.genome): $!\n";
 open OUT, '>', "pihat_failed_samples.txt";
-print "Reading PLINK .genome file pihat_0.125.genome\n";
+print "Reading PLINK .genome file pihat_min0.125.genome\n";
+
+print "For pair of samples which are unrelated check which sample has the highest missingness and print it to pihat_failed_samples.txt...\n";
 while(<GENOME>){
     s/^\s+//;
     my @fields = split /\s+/, $_;
- 	if($fields[9] > 0.125){
+	if($fields[9] > 0.125){
  		if($imiss{$fields[0]}{$fields[1]}>$imiss{$fields[2]}{$fields[3]}){
  			unless($removed{$fields[0]}{$fields[1]}){
  				print OUT "$fields[0] $fields[1]\n";
@@ -42,6 +57,4 @@ while(<GENOME>){
  		}
  	}
 }
-    
-	
-
+print "Done!\n";
