99242bf4efbb77a0539ba18b0b52fd99a7f06a0f
ChristinaVasil <39655952+ChristinaVasil@users.noreply.github.com>
2021-02-08 14:01:46 +0200

    Fixed labels in hwe.R
    Former-commit-id: 26040a5742c35d18a10247625cc56266b66b38ae

--- bin/plot_hwe.R
+++ bin/plot_hwe.R
@@ -12,8 +12,10 @@ read.table(args[[1]], header = T) %>%
   as_tibble(.) -> hwe
 
 hwe %>%
-  ggplot(., aes(x = P, y = Variant count)) +
+  ggplot(., aes(x = P)) +
   geom_histogram() +
   theme_linedraw() +
+  xlab("P-value") + 
+  ylab("Variant count")+
   ggtitle(paste("Hardy-Weinberg Equilibrium (HWE) ", args[[2]]))
 ggsave(paste0(args[[1]], ".png"))
