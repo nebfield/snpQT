935ff0abd24a87e8ef4d7f0e0cbc9214263e1bfa
ChristinaVasil <christine.vasil1@gmail.com>
2021-02-27 19:22:11 +0000

    fixed integers in plot_log.R

--- bin/plot_logs.R
+++ bin/plot_logs.R
@@ -8,6 +8,16 @@ library("tidyverse")
 
 args <- commandArgs(trailingOnly = TRUE)
 
+# A function factory for getting integer y-axis values.
+integer_breaks <- function(n = 5, ...) {
+    fxn <- function(x) {
+        breaks <- floor(pretty(x, n, ...))
+        names(breaks) <- attr(breaks, "labels")
+        breaks
+    }
+    return(fxn)
+}
+
 read_delim(args[[1]], delim = " ") %>%
     mutate(variants = as.integer(variants)) %>%
     mutate(samples = as.integer(samples)) %>%
@@ -16,18 +26,18 @@ read_delim(args[[1]], delim = " ") %>%
     mutate(stage = fct_reorder(stage, num)) -> df
 
 df %>% 
-  ggplot(., aes(x = stage, y = variants, group = 1)) + 
+  ggplot(., aes(x = stage, y = as.integer(variants), group = 1)) + 
   geom_point() + 
-  scale_y_continuous(breaks= pretty_breaks())+
+  scale_y_continuous(breaks= integer_breaks())+
   geom_line() + 
   theme_linedraw() +
   labs(x = "Stage", y = "Number of variants")
 ggsave(paste0(args[[2]], "_variants.png"))
 
 df %>% 
-  ggplot(., aes(x = stage, y = samples, group = 1)) + 
+  ggplot(., aes(x = stage, y = as.integer(samples), group = 1)) + 
   geom_point() + 
-  scale_y_continuous(breaks= pretty_breaks())+
+  scale_y_continuous(breaks= integer_breaks())+
   geom_line() + 
   theme_linedraw() +
   labs(x = "Stage", y = "Number of samples")
